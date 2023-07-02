3d4c4fcb39715be0299e803598f3150cf14049d8
jdanprad0 <josedanielalves018@gmail.com>
2023-07-01 22:17:55 -0300

    finalizacoes

--- data.js
+++ data.js
@@ -1,26 +1,19 @@
 require("dotenv").config();
 
-function getCollection(client, collection) {
+function getCollection(collection, client) {
   const db = client.db("DB_ORDER_EASY");
   const col = db.collection(collection);
   return col;
 }
 
-async function insertMesaOcupada(numero, collection) {
-  const doc = {
-    numero: numero,
-  };
-
-  const existingMesa = await collection.findOne({ numero });
-  if (existingMesa) {
-    return { error: "Mesa já está ocupada" };
-  }
-
-  await collection.insertOne(doc).catch(() => {
-    return { error: "Erro ao reservar mesa" };
+async function getPasswordAndTypeFromCPF(collection, user) {
+  const res = await collection.findOne({ user: Number(user) }).catch(() => {
+    return { error: "Erro ao buscar usuário pelo CPF." };
   });
-
-  return true;
+  if (res.password && res.type) {
+    return { dbPassword: res.password, type: res.type };
+  }
+  return null;
 }
 
 async function queryMesasOcupadas(collection) {
@@ -35,46 +28,76 @@ async function queryMesasOcupadas(collection) {
   return docs.map((doc) => doc.numero);
 }
 
-async function registerOrders(id, order, amount, price, desk, collection) {
+async function insertMesaOcupada(numero, collection) {
   const doc = {
-    id: id,
-    order: order,
-    amount: amount,
-    price: price,
-    desk: desk,
+    numero: numero,
   };
 
+  const existingMesa = await collection.findOne({ numero });
+  if (existingMesa) {
+    return { error: "Mesa já está ocupada." };
+  }
+
+  await collection.insertOne(doc).catch(() => {
+    return { error: "Erro ao reservar mesa." };
+  });
+
+  return true;
+}
+
+async function freeTable(
+  collectionOrders,
+  collectionMesasOcupadas,
+  tableNumber
+) {
   try {
-    await collection.insertOne(doc);
+    await collectionOrders.deleteMany({ table: tableNumber });
+    await collectionMesasOcupadas.deleteMany({ numero: tableNumber });
     return true;
   } catch {
-    return { error: "Erro ao criar pedido" };
+    return { error: "Erro ao encerrar pedido e liberar mesa." };
   }
 }
 
-async function queryOrdersByDesk(collection, numberDesk) {
+async function queryOrdersByTable(collection, tableNumber) {
   try {
-    const orders = await collection.find({ desk: numberDesk }).toArray();
+    const orders = await collection.find({ table: tableNumber }).toArray();
     return orders;
   } catch {
-    return { error: "Erro ao buscar pedidos da mesa" };
+    return { error: "Erro ao buscar pedidos da mesa." };
+  }
+}
+
+async function registerOrder(collection, order) {
+  try {
+    await collection.insertOne(order);
+  } catch {
+    return { error: "Erro ao registrar pedido." };
+  }
+}
+
+async function removeOrder(collection, value) {
+  try {
+    await collection.deleteOne({ value: value });
+  } catch {
+    return { error: "Erro ao remover pedido." };
   }
 }
 
 async function hasEmployee(collection, user) {
   const query = { user: Number(user) };
   const res = await collection.findOne(query).catch(() => {
-    return { error: "Erro ao buscar usuário" };
+    return { error: "Erro ao buscar usuário." };
   });
   if (res) return true;
   return false;
 }
 
-async function insertEmployee(collection, name, user, position, password) {
+async function insertEmployee(collection, name, user, type, password) {
   const doc = {
     name: name,
     user: Number(user),
-    position: position,
+    type: type,
     password: password,
   };
 
@@ -84,35 +107,56 @@ async function insertEmployee(collection, name, user, position, password) {
     await collection
       .updateOne({ user: Number(user) }, { $set: doc })
       .catch(() => {
-        return { error: "Erro ao atualizar usuário" };
+        return { error: "Erro ao atualizar usuário." };
       });
   } else {
     await collection.insertOne(doc).catch(() => {
-      return { error: "Erro ao inserir usuário" };
+      return { error: "Erro ao inserir usuário." };
     });
   }
 
   return true;
 }
 
-async function getPasswordFromCPF(user, collection) {
-  const res = await collection.findOne({ user: Number(user) }).catch(() => {
-    return { error: "Erro ao buscar usuário usuário" };
-  });
-
-  if (res) {
-    return res.password;
+async function queryAllUsers(collection) {
+  try {
+    const allUsers = await collection.find().toArray();
+    return allUsers;
+  } catch {
+    return { error: "Erro ao buscar todos os usuários." };
   }
-  return null;
+}
+
+async function querySingleUser(collection, user) {
+  try {
+    const singleUser = await collection.findOne({ user: Number(user) });
+    return singleUser;
+  } catch {
+    return { error: "Erro ao buscar usuário." };
+  }
+}
+
+async function removeSingleUser(collection, user) {
+  try {
+    await collection.deleteOne({ user: Number(user) });
+    return true;
+  } catch {
+    return { error: "Erro ao remover usuário." };
+  }
 }
 
 module.exports = {
   getCollection,
-  insertMesaOcupada,
+  getPasswordAndTypeFromCPF,
   queryMesasOcupadas,
-  registerOrders,
-  queryOrdersByDesk,
+  insertMesaOcupada,
+  freeTable,
+  queryOrdersByTable,
+  registerOrder,
+  removeOrder,
   hasEmployee,
   insertEmployee,
-  getPasswordFromCPF,
+  queryAllUsers,
+  querySingleUser,
+  removeSingleUser,
 };
--- routes.js
+++ routes.js
@@ -7,13 +7,18 @@ const { client } = require("./mongodb");
 
 const {
   getCollection,
-  insertEmployee,
-  hasEmployee,
-  getPasswordFromCPF,
-  insertMesaOcupada,
+  getPasswordAndTypeFromCPF,
   queryMesasOcupadas,
-  registerOrders,
-  queryOrdersByDesk,
+  insertMesaOcupada,
+  freeTable,
+  queryOrdersByTable,
+  registerOrder,
+  removeOrder,
+  hasEmployee,
+  insertEmployee,
+  queryAllUsers,
+  querySingleUser,
+  removeSingleUser,
 } = require("./data");
 
 // rota para fazer login
@@ -29,10 +34,13 @@ router.post("/login", async (req, res) => {
   }
 
   // pegando "tabela" de usuarios
-  const collectionUsers = getCollection(client, "listUsers");
+  const collectionUsers = getCollection("listUsers", client);
 
   // verifica se o usuario esta no bando de dados pegando sua senha
-  const dbPassword = await getPasswordFromCPF(user, collectionUsers);
+  const { dbPassword, type } = await getPasswordAndTypeFromCPF(
+    collectionUsers,
+    user
+  );
 
   if (!dbPassword) {
     return res.status(404).json({ msg: "O usuário não foi encontrado :(" });
@@ -52,6 +60,7 @@ router.post("/login", async (req, res) => {
 
     return res.json({
       user: Number(user),
+      type: type.label,
       token: token,
     });
   } catch (err) {
@@ -70,62 +79,10 @@ router.post("/logout", (req, res) => {
   res.send("Logout realizado com sucesso!");
 });
 
-// reserva mesa
-router.post("/insertMesaOcupada", async (req, res) => {
-  const { numero } = req.body;
-
-  const collectionMesaOcupada = getCollection(client, "mesaOcupada");
-
-  try {
-    insertMesaOcupada(numero, collectionMesaOcupada);
-    res.status(200).json({ msg: "Mesa reservada com sucesso" });
-  } catch {
-    return res.status(422).json({ msg: "Erro ao reservar mesa" });
-  }
-});
-
-// busca todos os usuários
-router.get("/queryAllUsers", async (_, res) => {
-  const collectionUsers = getCollection(client, "listUsers");
-  try {
-    const allUsers = await collectionUsers.find().toArray();
-
-    res.status(200).json(allUsers);
-  } catch {
-    return res.status(422).json({ msg: "Erro ao retornar usuários" });
-  }
-});
-
-// busca um único usuário
-router.post("/querySingleUser", async (req, res) => {
-  const { user } = req.body;
-
-  const collectionUsers = getCollection(client, "listUsers");
-
-  try {
-    const singleUser = await collectionUsers.findOne({ user: Number(user) });
-    res.status(200).json(singleUser);
-  } catch {
-    return res.status(422).json({ msg: "Erro ao encontrar usuário" });
-  }
-});
-
-// remove um único usuário
-router.post("/removeSingleUser", async (req, res) => {
-  const { userRemove } = req.body;
-  const collectionUsers = getCollection(client, "listUsers");
-  try {
-    await collectionUsers.deleteOne({ user: Number(userRemove) });
-    res.status(200).json({ msg: "Usuário deletado com sucesso" });
-  } catch {
-    return res.status(422).json({ msg: "Erro ao remover usuário" });
-  }
-});
-
 // busca mesas ocupadas
 router.get("/queryMesasOcupadas", async (_, res) => {
   try {
-    const collection = getCollection(client, "mesaOcupada");
+    const collection = getCollection("mesaOcupada", client);
     const mesaOcupada = await queryMesasOcupadas(collection);
     res.status(200).json(mesaOcupada);
   } catch (err) {
@@ -133,66 +90,75 @@ router.get("/queryMesasOcupadas", async (_, res) => {
   }
 });
 
-// registra pedidos de uma mesa
-router.post("/registerOrder", async (req, res) => {
-  const { pedidos, desk } = req.body;
-  const collectionRegisterOrder = getCollection(client, "registerOrder");
+// reserva mesa
+router.post("/insertMesaOcupada", async (req, res) => {
+  const { numero } = req.body;
+
+  const collectionMesaOcupada = getCollection("mesaOcupada", client);
 
   try {
-    await collectionRegisterOrder.deleteMany({ desk: desk });
-
-    for (const pedido of pedidos) {
-      const { id, order, amount, price, desk } = pedido;
-
-      await registerOrders(
-        id,
-        order,
-        amount,
-        price,
-        desk,
-        collectionRegisterOrder
-      );
-    }
-
-    res.status(200).json({ msg: "Pedido realizado com sucesso!" });
+    await insertMesaOcupada(collectionMesaOcupada, numero);
+    res.status(200).json({ msg: "Mesa reservada com sucesso" });
   } catch {
-    return res.status(472).json({ msg: "Erro ao criar pedido" });
-  }
-});
-
-// busca todos os pedidos de uma mesa
-router.post("/queryOrdersByDesk", async (req, res) => {
-  const { numberDesk } = req.body;
-
-  try {
-    const collection = getCollection(client, "registerOrder");
-    const orders = await queryOrdersByDesk(collection, numberDesk);
-    res.status(200).json(orders);
-  } catch {
-    res.status(422).json({ msg: "Erro ao buscar pedidos da mesa" });
+    return res.status(422).json({ msg: "Erro ao reservar mesa" });
   }
 });
 
 // libera mesa e exclui seus pedidop
-router.post("/freeDesk", async (req, res) => {
-  const { numberDesk } = req.body;
+router.post("/freeTable", async (req, res) => {
+  const { tableNumber } = req.body;
 
   try {
-    const collectionOrders = getCollection(client, "registerOrder");
-    const collectionMesasOcupadas = getCollection(client, "mesaOcupada");
-
-    await collectionOrders.deleteMany({ desk: numberDesk });
-    await collectionMesasOcupadas.deleteMany({ numero: numberDesk });
-
+    const collectionOrders = getCollection("orders", client);
+    const collectionMesasOcupadas = getCollection("mesaOcupada", client);
+    await freeTable(collectionOrders, collectionMesasOcupadas, tableNumber);
     res.status(200).json({ msg: "Pedido finalizado com sucesso! " });
   } catch {
     res.status(422).json({ msg: "Erro ao encerrar pedido" });
   }
 });
 
+// busca todos os pedidos de uma mesa
+router.post("/queryOrdersByTable", async (req, res) => {
+  const { tableNumber } = req.body;
+
+  try {
+    const collection = getCollection("orders", client);
+    const orders = await queryOrdersByTable(collection, tableNumber);
+    res.status(200).json(orders);
+  } catch {
+    res.status(422).json({ msg: "Erro ao buscar pedidos da mesa" });
+  }
+});
+
+// registra pedidos de uma mesa
+router.post("/registerOrder", async (req, res) => {
+  const { order } = req.body;
+  const collectionRegisterOrder = getCollection("orders", client);
+
+  try {
+    await registerOrder(collectionRegisterOrder, order);
+    res.status(200).json({ msg: "Pedido realizado com sucesso!" });
+  } catch {
+    return res.status(472).json({ msg: "Erro ao criar pedido" });
+  }
+});
+
+// remove um único usuário
+router.post("/removeOrder", async (req, res) => {
+  const { value } = req.body;
+  const collectionUsers = getCollection("orders", client);
+  try {
+    await removeOrder(collectionUsers, value);
+    res.status(200).json({ msg: "Pedido removido com sucesso" });
+  } catch {
+    return res.status(422).json({ msg: "Erro ao remover pedido" });
+  }
+});
+
 // cadastro e atualização de usuário
 router.post("/register", async (req, res) => {
-  const { formValues, editProfile } = req.body;
+  const { formValues, editUser } = req.body;
   const { name, password, passwordConfirm, type, user } = formValues;
 
   // validações de obrigatoriedade dos campos
@@ -223,10 +189,10 @@ router.post("/register", async (req, res) => {
     return res.status(422).json({ msg: "As senhas devem ser iguais!" });
   }
 
-  const collectionUsers = getCollection(client, "listUsers");
+  const collectionUsers = getCollection("listUsers", client);
 
   // verifica se o usuário já existe no banco de dados em caso de novo cadastro
-  if (!editProfile) {
+  if (!editUser) {
     const userExists = await hasEmployee(collectionUsers, user);
 
     if (userExists) {
@@ -241,8 +207,57 @@ router.post("/register", async (req, res) => {
   const passwordHash = await bcrypt.hash(password, salt);
 
   // insere o novo funcionario no banco de dados
-  insertEmployee(collectionUsers, name, user, type, passwordHash);
+  await insertEmployee(collectionUsers, name, user, type, passwordHash);
   res.status(201).json({ msg: "Usuário criado com sucesso! " });
+});
+
+// busca todos os usuários
+router.get("/queryAllUsers", async (_, res) => {
+  const collectionUsers = getCollection("listUsers", client);
+  try {
+    allUsers = await queryAllUsers(collectionUsers);
+    res.status(200).json(allUsers);
+  } catch {
+    return res.status(422).json({ msg: "Erro ao retornar usuários" });
+  }
+});
+
+// busca um único usuário
+router.post("/querySingleUser", async (req, res) => {
+  const { user } = req.body;
+
+  const collectionUsers = getCollection("listUsers", client);
+
+  try {
+    singleUser = await querySingleUser(collectionUsers, user);
+    res.status(200).json(singleUser);
+  } catch {
+    return res.status(422).json({ msg: "Erro ao encontrar usuário" });
+  }
+});
+
+// remove um único usuário
+router.post("/removeSingleUser", async (req, res) => {
+  const { userRemove } = req.body;
+  const collectionUsers = getCollection("listUsers", client);
+
+  const { user } = req.headers;
+
+  if (userRemove === 20103689 || userRemove === 20104135) {
+    return res.status(422).json({ msg: "Você não pode excluir este usuário." });
+  }
+
+  if (Number(user) === Number(userRemove)) {
+    return res
+      .status(422)
+      .json({ msg: "Você não pode excluir o seu usuário." });
+  }
+  try {
+    await removeSingleUser(collectionUsers, userRemove);
+    res.status(200).json({ msg: "Usuário deletado com sucesso" });
+  } catch {
+    return res.status(422).json({ msg: "Erro ao remover usuário" });
+  }
 });
 
 module.exports = router;
