bb1719f171f444f1d6d3cb03352723a08f07978f
jdanprad0 <josedanielalves018@gmail.com>
2023-07-02 21:11:48 -0300

    imagem chef

--- src/view/FinalSummary.tsx
+++ src/view/FinalSummary.tsx
@@ -1,10 +1,13 @@
 /** @jsxImportSource @emotion/react */
 import { VFlow } from "bold-ui";
 import React, { Fragment, useEffect, useState } from "react";
+import { css } from "@emotion/react";
 import { AccordionFinalSummary } from "../components/AccordionFinalSummary";
 import { Header } from "../components/Header";
 import { PageContainer } from "../components/PageContainer";
 import api from "../api";
+
+const chef = require("../img/chef.png");
 
 export function FinalSummary() {
   const [mesasOcupadas, setMesasOcupadas] = useState([]);
@@ -32,9 +35,43 @@ export function FinalSummary() {
       <Header title="Finalizar"></Header>
       <PageContainer>
         <VFlow vSpacing={2}>
-          {mesasOcupadas.map((value) => renderRepeatedContent(value))}
+          {mesasOcupadas.length === 0 && (
+              <h1>Nenhuma mesa foi reservada.</h1>
+            ) && (
+              <>
+                <div css={stylesDivMesasVazias}>
+                  <img src={chef} alt="Chef" css={stylesImageChef} />
+                </div>
+                <h2 css={stylesTextoChef}>
+                  Nenhuma mesa
+                  <br />
+                  foi reservada.
+                </h2>
+              </>
+            )}
+          {mesasOcupadas &&
+            mesasOcupadas.map((value) => renderRepeatedContent(value))}
         </VFlow>
       </PageContainer>
     </Fragment>
   );
 }
+
+const stylesDivMesasVazias = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const stylesImageChef = css`
+  width: auto;
+  max-width: 50%;
+`;
+
+const stylesTextoChef = css`
+  margin-top: 1.5rem;
+  font-weight: bold;
+  word-wrap: break-word;
+  text-align: center;
+  font-style: italic;
+`;
